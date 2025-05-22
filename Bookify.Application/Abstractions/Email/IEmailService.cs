28c235e089e6c4ed148de29c0908db7f31118fab
natia02 <natia2.pruidze@outlook.com>
2025-05-22 19:41:43 +0400

    adding some application layer code

---
+++ Bookify.Application/Abstractions/Email/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace Bookify.Application.Abstractions.Email;
+
+public interface IEmailService
+{
+    Task SendAsync(Domain.Users.Email to, string subject, string body);
+}
---
+++ Bookify.Application/Bookings/ReserveBooking/BookingReservedDomainEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using Bookify.Application.Abstractions.Email;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Bookings.Events;
+using Bookify.Domain.Users;
+using MediatR;
+
+namespace Bookify.Application.Bookings.ReserveBooking;
+
+internal sealed class BookingReservedDomainEventHandler(
+    IBookingRepository bookingRepository,
+    IUserRepository userRepository,
+    IEmailService emailService)
+    : INotificationHandler<BookingReserveDomainEvent>
+{
+    public async Task Handle(BookingReserveDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var booking = await bookingRepository.GetByIdAsync(notification.BookingId, cancellationToken);
+        
+        if (booking is null)
+        {
+            return;
+        }
+        
+        var user = await userRepository.GetByIdAsync(booking.UserId, cancellationToken);
+        
+        if (user is null)
+        {
+            return;
+        }
+        
+        await emailService.SendAsync(user.Email, "Booking Reserved!",
+            "You have 10 minutes to confirm your booking. ");
+    }
+}
